192a203aaaa6d0a17e8b02c1372bd0b770155658
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-12-25 10:06:49 +0100

    Prepare Windows standalone project

--- src/Vocup.Win32/Properties/AssemblyInfo.cs
+++ src/Vocup.Win32/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vocup.Win32")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Vocup")]
+[assembly: AssemblyDescription("Kostenloser Vokabeltrainer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vocup.Win32")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("VectorData")]
+[assembly: AssemblyProduct("Vocup")]
+[assembly: AssemblyCopyright("Copyright © 2011 Florian Amstutz, © 2018 Daniel Lerch.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.2.0")]
+[assembly: AssemblyFileVersion("1.5.2.0")]
+[assembly: NeutralResourcesLanguage("de")]
