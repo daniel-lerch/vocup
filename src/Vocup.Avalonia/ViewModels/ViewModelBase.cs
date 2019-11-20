a28b86c088900ab4d805df7a38cd2647da8415e2
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-11-20 16:26:29 +0530

    Add experimental Vocup.Avalonia project

---
+++ src/Vocup.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Vocup.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
