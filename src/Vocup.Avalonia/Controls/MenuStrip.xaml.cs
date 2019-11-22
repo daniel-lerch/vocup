aad206f2eae7821e2941161e3c85a9e255ca3fe0
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-11-22 16:16:45 +0530

    Implement basic MenuStrip and ToolBar

---
+++ src/Vocup.Avalonia/Controls/MenuStrip.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Vocup.Avalonia.Controls
+{
+    public class MenuStrip : UserControl
+    {
+        public MenuStrip()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/Vocup.Avalonia/Controls/ToolBar.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Vocup.Avalonia.Controls
+{
+    public class ToolBar : UserControl
+    {
+        public ToolBar()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
