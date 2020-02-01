e19b47c3e91e928d212b0140e189010aee7f90f7
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-02-01 12:48:01 +0530

    Update copyright information and fix build error

--- src/Vocup/IO/VocupFile.cs
+++ src/Vocup/IO/VocupFile.cs
@@ -33,19 +33,25 @@ namespace Vocup.IO.Internal
         protected string ReadFile(string path)
         {
             byte[] ciphertext;
-            using FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
-            if (file.ReadByte() == 0x50 && file.ReadByte() == 0x4B && file.ReadByte() == 0x03 && file.ReadByte() == 0x04)
-                throw new NotSupportedException($"The file {path} cannot be opened by Vocup. A later version might be required.");
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                if (file.ReadByte() == 0x50 && file.ReadByte() == 0x4B && file.ReadByte() == 0x03 && file.ReadByte() == 0x04)
+                    throw new NotSupportedException($"The file {path} cannot be opened by Vocup. A later version might be required.");
 
-            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
-                ciphertext = Convert.FromBase64String(reader.ReadToEnd());
+                file.Position = 0;
 
-            using MemoryStream plainstream = new MemoryStream();
-            using ICryptoTransform transform = csp.CreateDecryptor();
-            using CryptoStream cipherstream = new CryptoStream(plainstream, transform, CryptoStreamMode.Write);
-            cipherstream.Write(ciphertext, 0, ciphertext.Length);
-            cipherstream.FlushFinalBlock();
-            return Encoding.UTF8.GetString(plainstream.ToArray());
+                using (StreamReader reader = new StreamReader(file, Encoding.UTF8))
+                    ciphertext = Convert.FromBase64String(reader.ReadToEnd());
+            }
+
+            using (MemoryStream plainstream = new MemoryStream())
+            using (ICryptoTransform transform = csp.CreateDecryptor())
+            using (CryptoStream cipherstream = new CryptoStream(plainstream, transform, CryptoStreamMode.Write))
+            {
+                cipherstream.Write(ciphertext, 0, ciphertext.Length);
+                cipherstream.FlushFinalBlock();
+                return Encoding.UTF8.GetString(plainstream.ToArray());
+            }
         }
 
         /// <summary>
@@ -61,13 +67,15 @@ namespace Vocup.IO.Internal
         {
             byte[] buffer = Encoding.UTF8.GetBytes(content);
 
-            using StreamWriter writer = new StreamWriter(path, false, Encoding.UTF8);
-            using MemoryStream cipherstream = new MemoryStream();
-            using ICryptoTransform transform = csp.CreateEncryptor();
-            using CryptoStream plainstream = new CryptoStream(cipherstream, transform, CryptoStreamMode.Write);
-            plainstream.Write(buffer, 0, buffer.Length);
-            plainstream.FlushFinalBlock();
-            writer.Write(Convert.ToBase64String(cipherstream.ToArray()));
+            using (StreamWriter writer = new StreamWriter(path, false, Encoding.UTF8))
+            using (MemoryStream cipherstream = new MemoryStream())
+            using (ICryptoTransform transform = csp.CreateEncryptor())
+            using (CryptoStream plainstream = new CryptoStream(cipherstream, transform, CryptoStreamMode.Write))
+            {
+                plainstream.Write(buffer, 0, buffer.Length);
+                plainstream.FlushFinalBlock();
+                writer.Write(Convert.ToBase64String(cipherstream.ToArray()));
+            }
         }
     }
 }
