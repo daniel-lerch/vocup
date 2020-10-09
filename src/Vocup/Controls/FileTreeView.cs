ba517cc7c8f88391f651845bca0ca2c80b7d4208
Daniel Lerch <daniel.p.lerch@gmail.com>
2020-10-09 23:33:01 +0200

    Add missing translation and hide directory search below Windows 10

--- src/Vocup/Controls/FileTreeView.cs
+++ src/Vocup/Controls/FileTreeView.cs
@@ -92,7 +92,7 @@ namespace Vocup.Controls
         public bool BrowseButtonVisible
         {
             get => BrowseButton.Visible;
-            set => BrowseButton.Visible = !AppInfo.IsMono && value;
+            set => BrowseButton.Visible = AppInfo.IsWindows10 && !AppInfo.IsMono && value;
         }
 
         [Browsable(true), EditorBrowsable(EditorBrowsableState.Always)]
