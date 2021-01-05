20af850b3316ef546f37d7e60645993f7df15f25
Daniel Lerch <daniel.p.lerch@gmail.com>
2021-01-05 22:58:13 +0100

    Implement main sidebar with color transitions This is the third design draft for Vocup 2.0 and will replace MenuPage.axaml

--- src/Vocup.Avalonia/Views/MainWindow.axaml.cs
+++ src/Vocup.Avalonia/Views/MainWindow.axaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.Animation;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using System;
+using Vocup.Avalonia.Controls;
 
 namespace Vocup.Avalonia.Views
 {
@@ -8,6 +12,18 @@ namespace Vocup.Avalonia.Views
         public MainWindow()
         {
             InitializeComponent();
+
+            Style style = new Style(x => x.OfType<Button>());
+            style.Setters.Add(new Setter(TransitionsProperty, new Transitions()
+            {
+                new SolidColorBrushTransition
+                {
+                    Duration = TimeSpan.FromMilliseconds(250),
+                    Property = BackgroundProperty,
+                }
+            }));
+
+            Styles.Add(style);
         }
 
         private void InitializeComponent()
