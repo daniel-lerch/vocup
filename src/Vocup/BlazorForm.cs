e0c0e09942f68a5cd93f7e2a8175e0c55e916a14
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-05-05 19:40:10 +0200

    Follow Blazor hybrid tutorial (interactions not working)

---
+++ src/Vocup/BlazorForm.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Forms;
+
+namespace Vocup
+{
+    public partial class BlazorForm : Form
+    {
+        public BlazorForm()
+        {
+            InitializeComponent();
+
+            var services = new ServiceCollection();
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+            blazorWebView1.RootComponents.Add<Counter>("#app");
+        }
+    }
+}
