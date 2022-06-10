fdc0c1ca6f8842aa7b58c89437731fe94c86a688
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-06-10 10:06:21 +0200

    Extend MAUI project with ReactiveUI

--- src/Vocup.Maui/MainPage.xaml.cs
+++ src/Vocup.Maui/MainPage.xaml.cs
@@ -2,26 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-        BatteryViewModel Battery { get; }
-
         public MainPage()
         {
             InitializeComponent();
-            Battery = new BatteryViewModel();
-            BindingContext = this;
+            BindingContext = new MainPageViewModel();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 }
---
+++ src/Vocup.Maui/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace Vocup.Maui
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        private ObservableAsPropertyHelper<string> _buttonText;
+        private int _counter;
+
+        public MainPageViewModel()
+        {
+            Battery = new BatteryViewModel();
+            ButtonCommand = ReactiveCommand.Create(() => Count++);
+            this.WhenAnyValue(x => x.Count, counter => counter switch
+            {
+                0 => "Click me",
+                1 => $"Clicked 1 time",
+                _ => $"Clicked {counter} times"
+            }).ToProperty(this, x => x.ButtonText, out _buttonText);
+        }
+
+        public BatteryViewModel Battery { get; }
+        public ICommand ButtonCommand { get; }
+        public int Count
+        {
+            get => _counter;
+            set => this.RaiseAndSetIfChanged(ref _counter, value);
+        }
+        public string ButtonText => _buttonText.Value;
+    }
+}
