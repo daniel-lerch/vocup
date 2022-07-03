4c673089b51d3f359c4e4f46ed219893db7f1c95
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2022-07-03 22:42:29 +0200

    Data binding via DependencyProperty

--- src/Vocup/Controls/SettingsToggleSwitch.xaml.cs
+++ src/Vocup/Controls/SettingsToggleSwitch.xaml.cs
@@ -1,44 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Vocup.Controls
+namespace Vocup.Controls;
+
+/// <summary>
+/// Interaction logic for SettingsToggleSwitch.xaml
+/// </summary>
+public partial class SettingsToggleSwitch : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SettingsToggleSwitch.xaml
-    /// </summary>
-    public partial class SettingsToggleSwitch : UserControl
+    public SettingsToggleSwitch()
     {
-        public SettingsToggleSwitch()
-        {
-            InitializeComponent();
-        }
-
-        public string Title
-        {
-            get => (string)GetValue(TitleProperty);
-            set => SetValue(TitleProperty, value);
-        }
-
-        public static readonly DependencyProperty TitleProperty =
-            DependencyProperty.Register(
-                name: nameof(Title),
-                propertyType: typeof(string),
-                ownerType: typeof(SettingsToggleSwitch),
-                new PropertyMetadata(defaultValue: string.Empty));
-
-        public string Description { get; set; }
-        public bool IsChecked { get; set; }
+        InitializeComponent();
+        layoutRoot.DataContext = this;
     }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            name: nameof(Title),
+            propertyType: typeof(string),
+            ownerType: typeof(SettingsToggleSwitch),
+            new PropertyMetadata(defaultValue: string.Empty));
+
+    public string Description
+    {
+        get => (string)GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public static readonly DependencyProperty DescriptionProperty =
+        DependencyProperty.Register(
+            name: nameof(Description),
+            propertyType: typeof(string),
+            ownerType: typeof(SettingsToggleSwitch),
+            new PropertyMetadata(defaultValue: string.Empty));
+
+    public bool IsChecked
+    {
+        get => (bool)GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+
+    public static readonly DependencyProperty IsCheckedProperty =
+        DependencyProperty.Register(
+            name: nameof(IsChecked),
+            propertyType: typeof(bool),
+            ownerType: typeof(SettingsToggleSwitch),
+            new PropertyMetadata(defaultValue: false));
 }
