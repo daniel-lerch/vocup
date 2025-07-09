7e5389bf739892877a886addd3f8f234dc6e02f4
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2025-07-10 00:19:07 +0200

    Basic book view

---
+++ src/Vocup/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+
+namespace Vocup.Models;
+
+public class Book : ReactiveObject
+{
+    private string _motherTongue = string.Empty;
+    public string MotherTongue
+    {
+        get => _motherTongue;
+        set => this.RaiseAndSetIfChanged(ref _motherTongue, value);
+    }
+
+    private string _foreignLanguage = string.Empty;
+    public string ForeignLanguage
+    {
+        get => _foreignLanguage;
+        set => this.RaiseAndSetIfChanged(ref _foreignLanguage, value);
+    }
+
+    public ObservableCollection<Word> Words { get; } = [];
+}
--- src/Vocup/ViewModels/BookViewModel.cs
+++ src/Vocup/ViewModels/BookViewModel.cs
@@ -1,9 +1,38 @@
 ﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using Vocup.Models;
 
 namespace Vocup.ViewModels;
 
 public class BookViewModel : ViewModelBase
 {
+    private readonly Book _book;
+
+    public BookViewModel()
+    {
+        _book = new Book
+        {
+            MotherTongue = "German",
+            ForeignLanguage = "English"
+        };
+        _book.Words.Add(new Word(["Apfel"], ["apple"]));
+        _book.Words.Add(new Word(["Banane"], ["banana"]));
+        _book.Words.Add(new Word(["Kirsche"], ["cherry"]));
+        _book.Words.Add(new Word(["Birne"], ["pear"]));
+        _book.Words.Add(new Word(["Traube"], ["grape"]));
+        _book.Words.Add(new Word(["Brombeere"], ["blackberry"]));
+        _book.Words.Add(new Word(["Himbeere"], ["raspberry"]));
+        _book.Words.Add(new Word(["Pflaume"], ["plum"]));
+        _book.Words.Add(new Word(
+            ["Hänschen klein, geht allein\r\nIn die weite Welt hinein,\r\nStock und Hut steht ihm gut,\r\nIst auch wohlgemuth.\r\nAber Mutter weinet sehr,\r\nHat ja nun kein Hänschen mehr.\r\nWünsch dir Glück, sagt ihr Blick,\r\nKomm nur bald zurück!"],
+            ["Little Hans goes alone\r\nInto the wide world,\r\nStick and hat suit him well,\r\nIs also in good spirits.\r\nBut mother weeps a lot,\r\nNow she has no little one left.\r\nWish you luck, says her look,\r\nCome back soon!"]));
+        _book.Words.Add(new Word(
+            ["Vater unser im Himmel! Geheiligt werde dein Name. Dein Reich komme. Dein Wille geschehe, wie im Himmel, so auf Erden. Unser tägliches Brot gib uns heute. Und vergib uns unsere Schuld, wie auch wir vergeben unsern Schuldigern. Und führe uns nicht in Versuchung, sondern erlöse uns von dem Bösen. Denn dein ist das Reich und die Kraft und die Herrlichkeit in Ewigkeit. Amen."],
+            ["Our Father who art in heaven, hallowed be thy name; thy kingdom come; Thy will be done on earth as it is in heaven. Give us this day our daily bread, and forgive us our trespasses, as we forgive those who trespass against us, and lead us not into temptation, but deliver us from evil. For thine is the kingdom, and the power, and the glory, for ever and ever. Amen."]));
+    }
+
+    public ObservableCollection<Word> Words => _book.Words;
+
     private long _fileLength;
     public long FileLength
     {
