626648fd48ca36839ce0a8e579b327a001b1a855
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2025-08-19 23:35:18 +0200

    PracticeState using Zipf's law weights

--- src/Vocup/ViewModels/SynonymViewModel.cs
+++ src/Vocup/ViewModels/SynonymViewModel.cs
@@ -1,5 +1,10 @@
-﻿using ReactiveUI;
+﻿using DynamicData;
+using DynamicData.Binding;
+using ReactiveUI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
 using Vocup.Models;
 
 namespace Vocup.ViewModels;
@@ -7,19 +12,55 @@ namespace Vocup.ViewModels;
 public class SynonymViewModel : ViewModelBase, IDisposable
 {
     private readonly ObservableAsPropertyHelper<string> valueHelper;
-    private readonly ObservableAsPropertyHelper<int> practicesCountHelper;
+    private readonly ObservableAsPropertyHelper<double> practiceStateHelper;
 
     public SynonymViewModel(Synonym parent)
     {
         valueHelper = parent.WhenAnyValue(s => s.Value).ToProperty(this, s => s.Value);
-        practicesCountHelper = parent.WhenAnyValue(s => s.Practices.Count).ToProperty(this, s => s.PracticesCount);
+        practiceStateHelper = parent.Practices.ToObservableChangeSet()
+            .ToCollection()
+            .Select(GetPracticeState)
+            .ToProperty(this, s => s.PracticeState, initialValue: 0.0);
     }
 
     public string Value => valueHelper.Value;
-    public int PracticesCount => practicesCountHelper.Value;
+    public double PracticeState => practiceStateHelper.Value;
+
+    private static double GetPracticeState(IReadOnlyCollection<Practice> practices)
+    {
+        // Zipf's law weighted average for the last 5 practices
+        int lastN = 5;
+
+        double values = 0.0, weights = 0.0;
+        int counter = 1;
+
+        foreach (Practice practice in practices.Reverse().Take(lastN))
+        {
+            double weight = 1.0 / counter;
+            double result = practice.Result switch
+            {
+                PracticeResult2.Correct => 1.0,
+                PracticeResult2.PartlyCorrect => 0.5,
+                PracticeResult2.Wrong => 0.0,
+                _ => throw new ArgumentOutOfRangeException(null, practice.Result, "Invalid practice result")
+            };
+            values += result * weight;
+            weights += weight;
+            counter++;
+        }
+
+        for (; counter <= lastN; counter++)
+        {
+            // Assume 0.0 for missing practices
+            weights += 1.0 / counter;
+        }
+
+        return values / weights;
+    }
 
     public void Dispose()
     {
         valueHelper.Dispose();
+        practiceStateHelper.Dispose();
     }
 }
