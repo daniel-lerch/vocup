2b41e1a23959933858db06977c1835ab6b6ee7c5
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2025-08-31 16:23:51 +0200

    Colored practice state UI

---
+++ src/Vocup/Views/PracticeStateToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace Vocup.Views;
+
+public class PracticeStateToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double doubleValue)
+        {
+            byte red = (byte)(255 * (1.0 - doubleValue));
+            byte green = (byte)(255 * doubleValue);
+            return new SolidColorBrush(Color.FromRgb(red, green, 0));
+        }
+        else
+        {
+            throw new ArgumentException("Unsupported type", nameof(value));
+        }
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
