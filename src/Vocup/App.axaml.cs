f322e03b22d210ee659506cdceadadb717d74d13
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2026-01-09 22:14:54 +0100

    Replace legacy Avalonia DevTools with Accelerate DevTools

--- src/Vocup/App.axaml.cs
+++ src/Vocup/App.axaml.cs
@@ -14,6 +14,9 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
     }
 
     public void OpenFile(Uri path)
