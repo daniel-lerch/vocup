854d8832f96d415372fb1571db3c641026e8efbf
Daniel Lerch <36048059+daniel-lerch@users.noreply.github.com>
2026-01-29 20:12:18 +0100

    Add empty practice view

---
+++ src/Vocup/ViewModels/PracticeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vocup.ViewModels;
+
+public class PracticeViewModel
+{
+}
---
+++ src/Vocup/Views/PracticeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Vocup;
+
+public partial class PracticeView : UserControl
+{
+    public PracticeView()
+    {
+        InitializeComponent();
+    }
+}
